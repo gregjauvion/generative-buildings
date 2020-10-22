7f2d14e63f0be48d537f18d5c0f64a8738aee607
gregoire <gregoire@plumelabs.com>
2020-10-22 22:47:20 +0200

    increase resolution

--- dataset.py
+++ dataset.py
@@ -6,28 +6,36 @@ import matplotlib.pyplot as plt
 import cv2
 
 
-ROOT = 'data/raw/arcDataset'
+ARC_DATASET_ROOT = 'data/arc_dataset_raw/arcDataset'
+
+JFR_DATASET = [f'data/jfr_dataset/pour-gregoire{i}' for i in range(1, 4)]
 
 
-def get_images_paths():
+def get_arc_dataset():
     """
-    Get all paths to the images
+    Generator on Arc Dataset images
     """
 
-    images_path = []
-    for p in os.listdir(ROOT):
-        p_dir = f'{ROOT}/{p}'
+    for p in os.listdir(ARC_DATASET_ROOT):
+        p_dir = f'{ARC_DATASET_ROOT}/{p}'
         if os.path.isdir(p_dir):
             for img in os.listdir(f'{p_dir}'):
-                images_path.append(f'{p_dir}/{img}')
-
-    return images_path
+                yield cv2.imread(f'{p_dir}/{img}')
 
 
-RESOLUTION = 256
+def get_jfr_dataset():
+    """
+    Generator on JFR dataset
+    """
+
+    for path in JFR_DATASET:
+        for p in sorted(os.listdir(path)):
+            if p!='.DS_Store':
+                yield p, cv2.imread(f'{path}/{p}')
 
 
-def resize_image(image):
+
+def resize_image(image, resolution):
     """
     Resize an image to the wanted resolution.
     Same factor is applied along x and y axis
@@ -38,16 +46,25 @@ def resize_image(image):
     # Transform into a square image
     if h>w:
         nb_before, nb_after = int((h-w)/2), h - w - int((h-w)/2)
-        before, after = np.zeros((h, nb_before, 3)), np.zeros((h, nb_after, 3))
-        image_t = np.concatenate((before, image, after), axis=1)
+        before, after = np.zeros((h, nb_before, 3)) + 255, np.zeros((h, nb_after, 3)) + 255
+        image_t = np.concatenate((before, image, after), axis=1).astype(np.uint8)
     elif h<w:
         nb_before, nb_after = int((w-h)/2), w - h - int((w-h)/2)
-        before, after = np.zeros((nb_before, w, 3)), np.zeros((nb_after, w, 3))
-        image_t = np.concatenate((before, image, after), axis=0)
+        before, after = np.zeros((nb_before, w, 3)) + 255, np.zeros((nb_after, w, 3)) + 255
+        image_t = np.concatenate((before, image, after), axis=0).astype(np.uint8)
     elif h==w:
         image_t = image
 
-    return cv2.resize(image_t, (RESOLUTION, RESOLUTION))
+    # Resize image
+    image_resized = cv2.resize(image_t, (resolution, resolution))
+
+    # Set uniform white background (255, 255, 255)
+    # It Background may be black or white, we set white everywhere to be consistent
+    border = (image_resized==255).all(axis=2) | (image_resized==0).all(axis=2)
+    border_rgb = np.repeat(np.expand_dims(border, -1), 3, axis=2)
+    image_resized[border_rgb] = 255
+
+    return image_resized
 
 
 def augment_image(image, plot=False):
@@ -71,13 +88,22 @@ def augment_image(image, plot=False):
 
 if __name__=='__main__':
 
+    # Loop on JFR images
+    for e, (name, img) in tqdm(enumerate(get_jfr_dataset()), total=7437):
+        print(e, name)
+        if not '.psd' in name:
+            image, image_flipped = augment_image(resize_image(img, 512))
+            cv2.imwrite(f'data/jfr_dataset_resized_augmented_512/{e}.jpg', image)
+            cv2.imwrite(f'data/jfr_dataset_resized_augmented_512/{e}_.jpg', image_flipped)
+
+
     # Loop on all images and resize them
-    images_paths = get_images_paths()
-    for e, img_path in tqdm(enumerate(images_paths), total=len(images_paths)):
-        image = cv2.imread(img_path, 1)
-        images = augment_image(resize_image(image))
+    #images_paths = get_images_paths()
+    #for e, img_path in tqdm(enumerate(images_paths), total=len(images_paths)):
+    #    image = cv2.imread(img_path, 1)
+        
+        #images = augment_image(resize_image(image))
+        #for ee, img in enumerate(images):
+        #    cv2.imwrite(f'data/resized_augmented/{e}_{ee}.jpg', img)
 
-        for ee, img in enumerate(images):
-            cv2.imwrite(f'data/resized/{e}_{ee}.jpg', img)
-
-
+    #    cv2.imwrite(f'data/resized/{e}.jpg', resize_image(image))
